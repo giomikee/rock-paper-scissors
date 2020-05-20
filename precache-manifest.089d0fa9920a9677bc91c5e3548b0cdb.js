c36a2e6694deb802724efeb2bd9540df73f34bdd
Gio Justiniano <giojustiniano@Gios-Air.home>
2020-05-20 12:34:48 +0200

    Updates

---
+++ precache-manifest.089d0fa9920a9677bc91c5e3548b0cdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66d49702eee6d63dfbef4f32f05ffa1b",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "8888116926d216f1fc71",
+    "url": "/rock-paper-scissors/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a0ff0b67b6b175b26c0d",
+    "url": "/rock-paper-scissors/static/css/main.eceb4f11.chunk.css"
+  },
+  {
+    "revision": "8888116926d216f1fc71",
+    "url": "/rock-paper-scissors/static/js/2.f939fc40.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/rock-paper-scissors/static/js/2.f939fc40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0ff0b67b6b175b26c0d",
+    "url": "/rock-paper-scissors/static/js/main.a09fa029.chunk.js"
+  },
+  {
+    "revision": "c51fc6cb4475530d031f",
+    "url": "/rock-paper-scissors/static/js/runtime-main.15e62861.js"
+  }
+]);
