3a6dec0b1283cdfb2a0e48617eae7cec10c41db4
Gio Justiniano <giojustiniano@Gios-Air.home>
2020-05-20 13:31:56 +0200

    Fix github pages redirects for single page apps

--- src/components/Router.js
+++ src/components/Router.js
@@ -11,14 +11,13 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import GameMode from './GameMode';
 import App from './App';
 import NotFound from './NotFound';
-import { HOME } from '../constants';
 
 export default function Router() {
 	return (
-		<BrowserRouter>
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
 			<Switch>
-				<Route exact path={HOME} component={GameMode} />
-				<Route path={`${HOME}/play/:gameId`} component={App} />
+				<Route exact path='/' component={GameMode} />
+				<Route path='/play/:gameId' component={App} />
 				<Route component={NotFound} />
 			</Switch>
 		</BrowserRouter>
